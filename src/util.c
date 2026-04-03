4311911c3cf5312370a8be676c250c348f7e9f30
ginja881 <plutocstars@gmail.com>
2026-04-03 03:36:10 -0600

    Fix

--- src/util.c
+++ src/util.c
@@ -25,18 +25,19 @@ void report_error(error_code error, string line, size_t line_pos, size_t char_po
 		break;
      }
 
-     printf(
-     	"\n \e[0;31m %s error on line %ld at position %ld \n \e[0m ",
+     fprintf(
+        stderr,
+     	"\n \e[0;31m%s error on line %ld at position %ld \e[0m\n ",
 	error_status, 
 	line_pos,
 	char_pos
      );
      
-     printf("%s\n", line + char_pos);
+     fprintf(stderr, "%s\n", line + char_pos);
 
-     for (size_t i = 0; i < (char_pos - 1); i++)
-        putchar('-');
+     for (size_t i = 0; i < char_pos; i++)
+        putc('-', stderr);
 
-     fprintf("^\n");
+     fprintf(stderr, "^\n");
      
 }
