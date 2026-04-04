e23fd0d44a4030fb39dc3748a4e7a73b2f9b6ad2
ginja881 <plutocstars@gmail.com>
2026-04-04 03:48:20 -0600

    oops

---
+++ src/lex/tokens.c
@@ -0,0 +1,58 @@
+#include "tokens.h"
+
+Token make_token(size_t line_pos, size_t char_pos, string data, size_t text_size, token token_type) {
+      Token new_token = (Token)checked_malloc(sizeof(struct Token_));
+      new_token->line_pos = line_pos;
+      new_token->char_pos = char_pos;
+      new_token->data = data;
+      new_token->text_size = text_size;
+      new_token->token_type = token_type;
+      
+      return new_token;
+}
+
+Lexer make_lexer() {
+       Lexer lexer = (Lexer)checked_malloc(sizeof(struct Lexer_));
+       lexer->queue = (TokenQueue)checked_malloc(sizeof(struct TokenQueue_));
+       lexer->queue->front = NULL;
+       lexer->queue->tail = NULL;
+
+       lexer->queue->size = 0;
+
+       lexer->current_pos = 0;
+       lexer->current_line = 1;
+
+       return lexer;
+}
+
+Token peek(TokenQueue queue) {
+     return queue->front;
+}
+int match(TokenQueue queue, token token_type) {
+    return queue->front->token_type == token_type ? TRUE : FALSE;
+}
+
+TokenQueue enqueue_token(TokenQueue queue, Token token) {
+      if (queue->size == 0) {
+           queue->front = token;
+	   queue->tail = queue->front;
+      }
+      else {
+          queue->tail->next = token;
+	  queue->tail = queue->tail->next;
+      }
+      queue->size++;
+      return queue;
+     
+}
+
+Token dequeue_token(TokenQueue queue) {
+      if (queue->size == 0) {
+           return NULL;
+      }
+
+      Token old_front = queue->front;
+      queue->front = queue->front->next;
+      queue->size--;
+      return old_front;
+}
---
+++ src/lex/tokens.h
@@ -0,0 +1,107 @@
+#ifndef __TOKENS_H__
+#define __TOKENS_H__
+
+
+#include "utils.h"
+
+// Main Enum for Tokens
+typedef enum {
+      // Operations
+      MUL,
+      ADD,
+      MOD,
+      SUB,
+      GT,
+      GT_EQ,
+      LT, 
+      LT_EQ,
+      COMPAR_EQ,
+      BIT_NOT,
+      BIT_AND,
+      BIT_RSHIFT,
+      BIT_LSHIFT,
+      BIT_OR,
+      // Control Flow statements
+      FOR,
+      TO,
+      IN,
+      WHILE,
+      DO,
+      IF,
+      ELSE,
+      THEN,
+      FUNCTION_DEF,
+      END,
+      LET,
+      // Expressions
+      NIL,
+      ID,
+      REAL,
+      NUM,
+      // Useful punctuation used for parsing/reporting errors that the LEXER should report
+      SEMI_COLON,
+      COLON,
+      L_PAREN,
+      R_PAREN,
+      // BRCKT = BRACKET
+      L_CURLY_BRCKT,
+      R_CURLY_BRCKT,
+      L_SQUARE_BRCKT,
+      R_SQUARE_BRRCK,
+      // Vars and Types
+      VAR_DEC,
+      TYPE_DEC,
+      VAR_ASSIGN,
+      TYPE_ASSIGN
+
+
+} token;
+
+// Raw Token Representation
+struct Token_ {
+     size_t line_pos;
+     size_t char_pos,
+     string data;
+     size_t text_size;
+     token token_type;
+
+     struct Token_* next;
+};
+typedef struct Token_* Token;
+
+// TokenQueue as classic data structure for parsing
+struct TokenQueue_ {
+      size_t size;
+      Token front;    
+      Token tail;
+};
+
+typedef struct TokenQueue_* TokenQueue;
+
+// entire lexer data structure to make parsing way easier
+struct Lexer_ {
+     TokenQueue queue;
+     size_t current_pos;
+     size_t current_line;
+};
+
+typedef struct Lexer_* Lexer;
+
+
+// Constructors
+Token make_token(size_t line_pos, size_t char_pos, string data, size_t text_size, token token_type);
+Lexer make_lexer();
+
+// Parser operations
+Token peek(TokenQueue queue);
+int match(TokenQueue queue, token token_type);
+
+
+// Dynamic operations
+Token dequeue_token(TokenQueue queue);
+TokenQueue enqueue_token(TokenQueue queue, Token token);
+
+
+
+#endif
+
